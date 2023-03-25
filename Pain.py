a322ce92ed13c790844c70fb53f4f8ce3328af3c
Tantalien <125830963+Tantalien@users.noreply.github.com>
2023-03-26 02:24:57 +0400

    Add files via upload

---
+++ Pain.py
@@ -0,0 +1,23 @@
+'''Напишите функцию sum_range(start, end), которая суммирует все целые числа
+от значения start до величины end включительно.
+Если пользователь задаст первое число большее чем второе, просто поменяйте их местами'''
+import math
+
+
+def sum_range(start, end):
+  if start < end:
+    a = sum(range(start, end + 1))
+
+  elif start > end:
+    a = sum(range(end, start + 1))
+
+  else:
+    print('Задайте диапазон чисел')
+
+  return a
+
+
+start = int(input("Введите первое число: "))
+end = int(input("Введите второе число: "))
+
+print(sum_range(start, end))
