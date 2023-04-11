be094b42196694b9645c4f762e399b20528847bd
Elena <123@123321.ru>
2023-04-11 22:52:03 +0400

    В процессе...

--- let_me_guess.py
+++ let_me_guess.py
@@ -11,7 +11,9 @@ a = 5  # variable very hot
 b = 10  # variable hot
 c = 20  # variable cold
 i = 2  # fire
+x = 0
 number = random.randint(1, 100)
+print(number)
 
 
 class Ui(QtWidgets.QDialog, Form):
@@ -21,47 +23,41 @@ class Ui(QtWidgets.QDialog, Form):
         self.setWindowIcon(QIcon('rainbow.png'))
         self.setWindowTitle('***amazing rainbow***')
         self.lineEdit.setPlaceholderText('Введите число')
-        #self.pushButton.clicked.connect(self.enter)
         self.pushButton.clicked.connect(self.variable)
 
-    def variable (self):                              # создала переменную, вводимую через lineEdit
-        z = self.lineEdit.text()
-        #self.label_2.setText(self.lineEdit.text())
-        print(z)
-        #попробовать эту функцию объединить с enter, чтобы она видела z. Или создать наследуемый класс
+    def variable(self):
+        global x
+        z = self.lineEdit.text()  # создала переменную, вводимую через lineEdit
+        x = z
+        print(x)
 
-
-'''
     def enter(self):
         global a
         global b
         global c
         global i
+        global x
         global number
-        z =  
-        #q = self.label_2.setText(self.lineEdit.text()) не верно
 
         while True:
-            #q = int(input('Угадай число (от 1 до 100):'))    #пока не понимаю, как сделать, чтобы q была связана с lineEdit
-            if number == q:
+            if number == x:
                 self.label_2.setText('Угадал! Ты умничка')
-            if number - i < q < number + i:
+            if number - i < x < number + i:
                 self.label_2.setText('Огонь!')
-            elif number - a < q < number + a:
+            elif number - a < x < number + a:
                 self.label_2.setText('Очень горячо')
-            elif number - b < q < number + b:
+            elif number - b < x < number + b:
                 self.label_2.setText('Горячо, но не сильно')
-            elif number - c < q < number + c:
+            elif number - c < x < number + c:
                 self.label_2.setText('Холодно')
-            elif guess > 100 or q < 1:
+            elif number > 100 or x < 1:
                 self.label_2.setText('Диапазон загаданного числа от 1 до 100')
             else:
                 self.label_2.setText('Лёд!')
-       '''
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = Ui()
     window.show()
     app.exec()
-
